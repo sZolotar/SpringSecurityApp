650da6ca41ce3b35d866cf7dc6ebb2076beafb55
sergii <sergeypf@gmail.com>
2017-03-29 16:15:54 +0300

    added DAO

---
+++ src/main/java/net/proselyte/sppringsecurityapp/dao/UserDao.java
@@ -0,0 +1,9 @@
+package net.proselyte.sppringsecurityapp.dao;
+
+import net.proselyte.sppringsecurityapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao  extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
