c46b26b803e8eaddf5f154ff79441bfdd9dedf76
sergii <sergeypf@gmail.com>
2017-03-29 17:23:01 +0300

    added Validator

---
+++ src/main/java/net/proselyte/sppringsecurityapp/validator/UserValidator.java
@@ -0,0 +1,55 @@
+package net.proselyte.sppringsecurityapp.validator;
+
+import net.proselyte.sppringsecurityapp.model.User;
+import net.proselyte.sppringsecurityapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * Validator for {@link net.proselyte.sppringsecurityapp.model.User} class
+ * Implements {@link Validator} interface.
+ *
+ * @author Zolotar Sergii
+ * @version 1.0
+ */
+
+@Component
+public class UserValidator  implements Validator{
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        User user = (User) target;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Required");
+
+        if(user.getUsername().length() < 8 || user.getUsername().length() > 32){
+            errors.rejectValue("username", "Size.loginForm.username");
+        }
+
+        if(userService.findByUsename(user.getUsername())!=null){
+            errors.rejectValue("username", "Duplicate.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Required");
+
+        if(user.getPassword().length() < 8 || user.getPassword().length() > 32){
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+        if(!user.getConfirmPassword().equals(user.getPassword())){
+            errors.rejectValue("confirmPassword", "Different.userForm.password");
+        }
+    }
+}
